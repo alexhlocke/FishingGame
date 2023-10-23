ea377d6543fc39e5b76ff7b409863e8033f7fd71
brielle <blacey@csumb.edu>
2023-10-23 10:40:09 -0700

    little updates

--- Assets/Bullet Hell/Scripts/Bullet.cs
+++ Assets/Bullet Hell/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
