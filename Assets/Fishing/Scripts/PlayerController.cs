ab2ef66888b95d48cfc10770be980a844a9ee4fb
Alex Locke <alexhlocke@gmail.com>
2023-10-23 11:38:56 -0700

    bounding area

---
+++ Assets/Fishing/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed = 1f;
+    
+
+    void Awake() {
+
+    }
+
+    void Update() {
+        
+    }
+}
