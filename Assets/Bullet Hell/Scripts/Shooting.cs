f0a6bfe0ae03d3721f2e7a1e7094ff604545676f
blacey15 <blacey@csumb.edu>
2023-10-23 17:26:09 -0700

    character shooting

--- Assets/Bullet Hell/Scripts/Shooting.cs
+++ Assets/Bullet Hell/Scripts/Shooting.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    private Camera mainCam;
+    private Vector3 mousePos;
+    
     public Transform firePoint;
+    
     public GameObject bulletPrefab;
     public float bulletForce = 15f;
+
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
