c0398445580abc51414182998f3d4649c2afc37f
brielle <blacey@csumb.edu>
2023-12-04 11:33:13 -0800

    dashing done

--- Assets/Bullet Hell/Scripts/PlayerMovement.cs
+++ Assets/Bullet Hell/Scripts/PlayerMovement.cs
@@ -1,31 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // private float horizontal;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    //public Camera cam;
-
     Vector2 movement;
-    //private Vector2 mousePos;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 10f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    [SerializeField] private TrailRenderer tr;
 
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
-       // mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+        }
     }
 
     void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
 
-        //Vector2 lookDirection = mousePos - rb.position;
-        //float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        //rb.rotation = angle;
+    private IEnumerator Dash(float dashDirectionx, float dashDirectiony)
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower * dashDirectionx, transform.localScale.x * dashingPower * dashDirectiony);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
